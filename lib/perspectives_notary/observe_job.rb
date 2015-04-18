8e18a2c9d9248a7b849f890b7e2a7e1376bde172
Adam Watkins <adam@stupidpupil.co.uk>
2015-04-18 01:20:11 +0100

    Auto reobserving

--- lib/perspectives_notary/observe_job.rb
+++ lib/perspectives_notary/observe_job.rb
@@ -4,14 +4,16 @@ module PerspectivesNotary
   class ObserveJob
     include SuckerPunch::Job
 
-    def perform(host, port, service_type)
+    def perform(host, port, service_type, auto_count = 0)
 
       service = "#{host}:#{port},#{service_type}"
+
       return if not PerspectivesNotary::Observation.observation_needed_for? service
       
       fingerprint = PerspectivesNotary::OpenSSLScanner.fingerprint(host, port)
       PerspectivesNotary::Observation.observe_fingerprint(service, fingerprint)
 
+      after(Config.auto_reobserve_interval) {ObserveJob.new.perform(host,port,service_type, auto_count + 1)} if auto_count < Config.auto_reobserve_count
     end
 
   end
