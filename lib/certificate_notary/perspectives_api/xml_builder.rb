4028ade75290079a2af00481c3b03bf8e0bb0c0f
Adam Watkins <adam@stupidpupil.co.uk>
2015-04-23 19:34:38 +0100

    First attempt at splitting out packed data building

--- lib/certificate_notary/perspectives_api/xml_builder.rb
+++ lib/certificate_notary/perspectives_api/xml_builder.rb
@@ -4,22 +4,34 @@ module CertificateNotary
 
       def self.xml_for_service(service, hash='md5')
         xml = ""
-        packed_data = ""
 
-        service.certificates_dataset.eager(:timespans).all.each do |certificate|
+        service.certificates.each do |certificate|
 
           fp = certificate[hash.to_sym].scan(/../).join(':')
-          timestamps = certificate.timespans
 
-          #
-          # Packed Data
-          #
+          xml << "<key fp=\"#{fp}\" type=\"ssl\">"
+          xml << certificate.timespans.map {|ts| "<timestamp end=\"#{ts[:end].to_i}\" start=\"#{ts[:start].to_i}\"/>" }.join("\n")
+          xml << "</key>"
 
-          head = [(timestamps.count >> 8) & 255, timestamps.count & 255, 0, 16, 3].pack('C'*5)
-          fp_bytes = fp.split(':').map {|h| [h.hex].pack('C')}.join
+        end
+
+        packed_data = XMLBuilder.packed_data_for_service(service, hash)
+        signature =  Base64.strict_encode64(Config.private_key.sign(OpenSSL::Digest::MD5.new, packed_data))
+
+        return "<notary_reply sig=\"#{signature}\" sig_type=\"rsa-md5\" version=\"1\">#{xml}</notary_reply>"
+
+      end
+
+      def self.packed_data_for_service(service, hash='md5')
+        packed_data = ""
+
+        service.certificates.each do |certificate|
+
+          head = [(certificate.timespans.count >> 8) & 255, certificate.timespans.count & 255, 0, 16, 3].pack('C'*5)
+          fp_bytes = certificate[hash.to_sym].scan(/../).map {|h| [h.hex].pack('C')}.join
           
           ts_bytes = ""
-          timestamps.each do |ts|
+          certificate.timespans.each do |ts|
             [ts[:start].to_i, ts[:end].to_i].each do |t|
               ts_bytes << [(t >> 24) & 255, (t >> 16) & 255, (t >> 8) & 255, t & 255].pack('C'*4)
             end
@@ -27,20 +39,9 @@ module CertificateNotary
 
           packed_data = (head + fp_bytes + ts_bytes) + packed_data
 
-          #
-          # Key Element
-          #
-
-          xml << "<key fp=\"#{fp}\" type=\"ssl\">"
-          xml << timestamps.map {|ts| "<timestamp end=\"#{ts[:end].to_i}\" start=\"#{ts[:start].to_i}\"/>" }.join("\n")
-          xml << "</key>"
-
         end
 
-        packed_data = service.id_string + [0].pack('C') + packed_data
-        signature =  Base64.strict_encode64(Config.private_key.sign(OpenSSL::Digest::MD5.new, packed_data))
-
-        return "<notary_reply sig=\"#{signature}\" sig_type=\"rsa-md5\" version=\"1\">#{xml}</notary_reply>"
+        return service.id_string + [0].pack('C') + packed_data
 
       end
 
