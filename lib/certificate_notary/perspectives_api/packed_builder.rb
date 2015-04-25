d4e5e0cabaa8c8a71486604d593c6f8059e103bd
Adam Watkins <adam@stupidpupil.co.uk>
2015-04-25 19:42:11 +0100

    Tidying

--- lib/certificate_notary/perspectives_api/packed_builder.rb
+++ lib/certificate_notary/perspectives_api/packed_builder.rb
@@ -6,16 +6,16 @@ module CertificateNotary
     class PackedBuilder
 
       def self.head_for_certificate(certificate)
-        [(certificate.timespans.count >> 8) & 255, certificate.timespans.count & 255, 0, 16, 3].pack('C'*5)
+        [(certificate.timespans.count >> 8) & 255, certificate.timespans.count & 255, 0].pack('C*')
       end
 
       def self.fingerprint_for_certificate(certificate, hash='md5')
-        certificate[hash.to_sym].scan(/../).map {|h| [h.hex].pack('C')}.join
+        ([16, 3] + certificate[hash.to_sym].scan(/../).map {|h| h.hex}).pack('C*')
       end
 
       def self.timestamp_for_time(time)
         t = time.to_i
-        [(t >> 24) & 255, (t >> 16) & 255, (t >> 8) & 255, t & 255].pack('C'*4)
+        [(t >> 24) & 255, (t >> 16) & 255, (t >> 8) & 255, t & 255].pack('C*')
       end
 
       def self.timestamps_for_timespan(timespan)
